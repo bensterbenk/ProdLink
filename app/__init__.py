596875b0177f811477ba066778091f1092a7b5ee
Ben Sterbenk <96723654+bensterbenk@users.noreply.github.com>
2023-12-14 07:36:07 -0500

    Converted to API
    Made changes to routes in order to provide a working API to interface with React

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -6,7 +8,19 @@ from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 import logging
 from logging.handlers import SMTPHandler
+<<<<<<< Updated upstream
 app = Flask(__name__, static_folder=Config.STATIC_FOLDER, static_url_path='/static')
+=======
+from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, \
+                               unset_jwt_cookies, jwt_required, JWTManager
+app = Flask(__name__, static_folder=Config.STATIC_FOLDER, static_url_path='/static')
+CORS(app, supports_credentials=True)  # Enable CORS for all routes
+app.config["JWT_SECRET_KEY"] = "please-remember-to-change-me"
+app.config['SESSION_COOKIE_SECURE'] = True
+jwt = JWTManager(app)
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+
+>>>>>>> Stashed changes
 login = LoginManager(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,12 @@
 import datetime
+import json
 import os
 import shutil
+import traceback
 from datetime import datetime
 from datetime import date
+from random import choice
+
 from flask import Blueprint, render_template, flash, redirect, request, url_for, current_app, Response, \
     send_from_directory
 from sqlalchemy import or_
@@ -12,14 +16,22 @@ from app import db
 from flask_login import current_user, login_user, login_required
 from app.models import User, Post, Tag, Comment
 from flask_login import logout_user
+from flask_cors import cross_origin
+from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, \
+                               unset_jwt_cookies, jwt_required, JWTManager
 
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
+<<<<<<< Updated upstream
 
     return render_template("index.html", title='Home Page')
+=======
+    data = {'message': 'This data comes from a JSON response.'}
+    return jsonify(data)
+>>>>>>> Stashed changes
 
 @app.route('/reset_db')
 def reset_db():
@@ -61,16 +73,60 @@ def populate_db():
 
     db.session.add_all([u1, u2])
     db.session.commit()
-    p1 = Post(title="Intro to Prodwizard", body='Im the prodwizard! boooo', user_id=u1.id, tags=[t1, t6, t11], audio_file='p1_audio.mp3')
-    p2 = Post(title="Only good takes", body='drill is all mid now LMFAOAOAO W lil mabu', user_id=u2.id, tags=[t2, t7, t12], audio_file='p2_audio.mp3')
+    prodwizard = User.query.filter_by(username='prodwizard').first()
 
-    db.session.add_all([p1, p2])
+    # Example titles and bodies for different types of tracks
+    example_titles = [
+        "Chill Vibes on a Sunday",
+        "Epic Guitar Solo",
+        "Lofi Beats for Studying",
+        "Soulful R&B Melody",
+        "Hype Hip-Hop Party",
+        "Experimental Drum and Bass",
+        "Acoustic Guitar Serenade",
+        "Piano Sonata in C Minor",
+        "Vocals of the Heart",
+        "Deep Bass Groove",
+        "Dynamic Drum Patterns",
+        "Melancholic Sad Symphony",
+        "Happy Uplifting Harmony",
+        "Energetic Hype Anthem",
+        "Mellow Evening Jazz"
+    ]
+
+    example_bodies = [
+        "Sit back and relax with this chill track. Perfect for a lazy Sunday afternoon.",
+        "Get ready for an epic guitar solo that will blow your mind!",
+        "Need some background music for studying? Check out these calming lofi beats.",
+        "Experience the soulful vibes of this R&B melody. Smooth and captivating.",
+        "Turn up the volume and join the hip-hop party! Hype beats and catchy lyrics await.",
+        "Dive into the world of experimental drum and bass with this mind-bending track.",
+        "Let the acoustic guitar serenade you with its soothing melodies.",
+        "Immerse yourself in the beauty of this piano sonata in C minor. A classical masterpiece.",
+        "Feel the emotions with powerful vocals that speak to the heart.",
+        "Groove to the deep bass and let the rhythm take over your senses.",
+        "Dynamic drum patterns that will keep you moving and grooving.",
+        "A symphony of melancholic notes that will tug at your heartstrings.",
+        "Need a mood boost? Listen to this happy and uplifting harmony.",
+        "Get ready for an energetic hype anthem that will lift your spirits.",
+        "Unwind with mellow evening jazz. Perfect for a laid-back atmosphere."
+    ]
+
+    for i in range(15):
+        # Choose a random tag for each post
+        tags = [choice([t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15])]
+
+        # Choose a random audio file for each post
+        audio_file = choice(["p1_audio.mp3", "p2_audio.mp3", "corleo_attempt.mp3", "jersey_1.mp3", "hard_but_soft.mp3"])
+
+        # Create and add the new post to the database
+        new_post = Post(title=example_titles[i], body=example_bodies[i], user_id=prodwizard.id, tags=tags,
+                        audio_file=audio_file)
+        db.session.add(new_post)
+
+    # Commit changes to the database
     db.session.commit()
     return render_template('index.html')
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -107,13 +163,14 @@ def user(username):
     form = EmptyForm()
     return render_template('user.html', user=active_user, form=form)
 
-@app.route('/samplesforum', methods=['GET','POST'])
+@app.route('/samplesforum', methods=['GET', 'POST'])
 def samples_forum():
     form = SearchForm()
     form.genretags.choices = [(a.id, a.name) for a in Tag.query.filter_by(tag_type='genre')]
     form.instrtags.choices = [(a.id, a.name) for a in Tag.query.filter_by(tag_type='instr')]
     form.moodtags.choices = [(a.id, a.name) for a in Tag.query.filter_by(tag_type='mood')]
     posts_display = Post.query.all()
+
     if form.validate_on_submit():
         posts_display = []
         search_text = form.title.data.lower()
@@ -139,7 +196,13 @@ def samples_forum():
                     or_(Post.title.ilike(f'%{search_text}%'), Post.body.ilike(f'%{search_text}%'))).all()
                 posts_display.extend(text_search_posts)
 
+    # Check if the request is a POST and return JSON response
+    if request.method == 'POST':
+        posts_data = [{'id': post.id, 'title': post.title, 'body': post.body, 'author': post.author.username} for post in posts_display]
+        return jsonify({'posts': posts_data})
 
+    # Render the template for GET request
+    return render_template('samplesforum.html', form=form, posts=posts_display)
 
     # Remove duplicate posts in case a post matches both tag and text criteria
     posts_display = list(set(posts_display))
@@ -153,11 +216,19 @@ def post(post_id):
     target_user = User.query.filter_by(id=target_post.user_id).first()
     if not target_post:
         return "Post not found", 404
+<<<<<<< Updated upstream
     if form.validate_on_submit():
         new_comment = Comment(body=form.body.data, user_id=current_user.id)
         db.session.add(new_comment)
         db.session.commit()
         target_post.comments.append(new_comment)
+=======
+    for tag in target_post.tags:
+        tag_list.append(tag.name)
+    for comment in target_post.comments:
+        comment_info = {"body": comment.body, "author": (User.query.filter_by(id=comment.user_id).first()).username}
+        comment_list.append(comment_info)
+>>>>>>> Stashed changes
 
     post_info = {
         'id': target_post.id,
@@ -178,7 +249,33 @@ def post(post_id):
 @app.route('/posts')
 def posts():
     posts = Post.query.all()
+<<<<<<< Updated upstream
     return render_template('posts.html', posts=posts)
+=======
+    posts_info = []
+    for post in posts:
+        target_user = User.query.filter_by(id=post.user_id).first()
+        tag_list = []
+        comment_list = []
+        for tag in post.tags:
+            tag_list.append(tag.name)
+        for comment in post.comments:
+            comment_info = {"body": comment.body, "author": (User.query.filter_by(id=comment.user_id).first()).username}
+            comment_list.append(comment_info)
+        post_info = {
+            'id': post.id,
+            "title": post.title,
+            "body": post.body,
+            "author_name": target_user.username,
+            "author_id": target_user.id,
+            "timestamp": post.timestamp,
+            "audio_file": post.audio_file,
+            "tags": tag_list,
+            "comments": comment_list
+        }
+        posts_info.append(post_info)
+    return jsonify(posts_info)
+>>>>>>> Stashed changes
 @app.route('/newpost', methods=['GET','POST'])
 def newpost():
     form = PostForm()
@@ -209,4 +306,173 @@ def about():
     return render_template('about.html')
 @app.route('/static/audio/<filename>')
 def serve_audio(filename):
+<<<<<<< Updated upstream
     return send_from_directory(app.root_path, f'static/audio/{filename}')
+=======
+    return send_from_directory(app.root_path, f'static/audio/{filename}')
+
+@app.route('/api/tags')
+def all_tags():
+    all_tags = Tag.query.all()
+    tag_data = [{'id': tag.id, 'name': tag.name, 'tag_type': tag.tag_type} for tag in all_tags]
+    return jsonify({'tags': tag_data})
+
+@app.route('/api/tags/<tag_id>', methods=['GET', 'POST'])
+def tag(tag_id):
+    tag = Tag.query.filter_by(id=tag_id).first()
+
+    tag_posts = tag.posts
+    tag_posts_ids = []
+    for post in tag_posts:
+        tag_posts_ids.append(post.id)
+    tag_info = {
+        'id':tag.id,
+        'name': tag.name,
+        'posts': tag_posts_ids,
+        'tag_type': tag.tag_type
+    }
+    return tag_info
+
+@app.route('/api/register', methods=['POST'])
+def register_user():
+    data = request.get_json()
+
+    # Extract registration data
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+
+    # Check if username or email already exists
+    if User.query.filter_by(username=username).first() or User.query.filter_by(email=email).first():
+        return jsonify({'error': 'Username or email already exists'}), 400
+
+    # Create a new user
+    new_user = User(username=username, email=email)
+    new_user.set_password(password)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'Registration successful'}), 201
+
+@app.route('/api/login', methods=['POST'])
+def login_user_route():
+    @app.route('/api/login', methods=['POST'])
+    def login():
+        data = request.get_json()
+
+        username = data.get('username')
+        password = data.get('password')
+
+        # Replace 'User' with the actual model you are using
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.check_password(password):
+            # Create JWT token and send it to the client
+            access_token = create_access_token(identity=username)
+            return jsonify(access_token=access_token), 200
+        else:
+            return jsonify(message='Invalid credentials'), 401
+@app.route('/api/logout')
+@login_required
+def logout_user_route():
+    # Log out the user
+    logout_user()
+
+    return jsonify({'message': 'Logout successful'}), 200
+
+
+@app.route('/api/newpost', methods=['POST'])
+def newpostapi():
+    try:
+        # Access form data using request.form
+        title = request.form.get('title')
+        post_body = request.form.get('post')
+
+        # Handle audio file
+        audio_file = request.files.get('audio_file')
+        print(audio_file)
+
+        # Convert user id to integer (assuming there is at least one user)
+        user_id = int(User.query.all()[0].id)
+
+        # Create a new post
+        post = Post(title=title, body=post_body, user_id=user_id)
+        post.save_audio(audio_file)
+
+        # Add tags to the post
+        tag_ids = json.loads(request.form.get('genretags', '[]')) + \
+                  json.loads(request.form.get('moodtags', '[]')) + \
+                  json.loads(request.form.get('instrtags', '[]'))
+
+        if not isinstance(tag_ids, (list, tuple)):
+            tag_ids = [tag_ids]
+
+        all_tags = Tag.query.all()
+        for tag in all_tags:
+            for tag_id in tag_ids:
+                if tag_id == tag.id:
+                    post.tags.append(tag)
+
+        # Save the post to the database
+        db.session.add(post)
+        db.session.commit()
+
+        return jsonify({'message': 'Post created successfully'}), 201
+
+    except Exception as e:
+        # Log the error traceback for debugging
+        app.logger.error(f"Error creating post: {e}")
+        app.logger.error(traceback.format_exc())
+
+        return jsonify({'error': 'Internal Server Error'}), 500
+@app.route('/api/check-auth')
+@cross_origin(supports_credentials=True)
+def check_authentication():
+    if current_user.is_authenticated:
+        return jsonify({'authenticated': True, 'username': current_user.username})
+    else:
+        return jsonify({'authenticated': False})
+
+@app.after_request
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.now(datetime.timezone.utc)
+        target_timestamp = datetime.timestamp(now + datetime.timedelta(minutes=30))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity())
+            data = response.get_json()
+            if type(data) is dict:
+                data["access_token"] = access_token
+                response.data = json.dumps(data)
+        return response
+    except (RuntimeError, KeyError):
+        # Case where there is not a valid JWT. Just return the original respone
+        return response
+@app.route('/token', methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    if email != "test" or password != "test":
+        return {"msg": "Wrong email or password"}, 401
+
+    access_token = create_access_token(identity=email)
+    response = {"access_token":access_token}
+    return response
+
+@app.route('/profile')
+@jwt_required()
+def my_profile():
+    response_body = {
+        "name": "Nagato",
+        "about" :"Hello! I'm a full stack developer that loves python and javascript"
+    }
+
+    return response_body
+@app.route("/logout", methods=["POST"])
+def logout():
+    response = jsonify({"msg": "logout successful"})
+    unset_jwt_cookies(response)
+    return response
+>>>>>>> Stashed changes
